2adabd645e246f8ee77736eac9f247a517761b8b
diennguyen90 <diennguyen90@yahoo.com>
2015-02-17 11:54:48 -0800

    new changes with partials

--- app3.js
+++ app3.js
@@ -1,22 +1,52 @@
-var app = angular.module('myApp', ['firebase']);
+var app = angular.module('myApp', ['ngRoute']);
 
-app.controller('MainController', function($scope, $firebase){
-		//creates a reference to Firebase
-	var ref = new Firebase("https://dienspokedex.firebaseIO.com");
+app.config(function($routeProvider){
+	$routeProvider
 
-	var sync = $firebase(ref);
-	var obj = sync.$asObject();
-	//
-	$scope.myList = sync.$asArray();
-	$scope.addBeer=function(){
-		//adds to your firebase
-		$scope.myList.$add($scope.beer);
-		$scope.beer= {};
-	};
-	$scope.removeBeer=function(){
-		$scope.myList.$remove($scope.beer);
-	}
-	
+	//route for home
+	.when('/', {
+		templateUrl : 'home.html',
+		controller : 'MainController'
+	});
+	//route for login
+	//.when('/login', {
+	//	templateUrl : 'login.html',
+	//	controller : 'LoginController'
+	//})
+	////route for register
+	//.when('/register', {
+	//	templateUrl : 'register.html',
+	//	controller : 'RegController'
+	//});
 
 });
 
+
+
+app.controller('MainController', function($scope, $firebase){
+		//creates a reference to Firebase
+	//var ref = new Firebase("https://dienspokedex.firebaseIO.com");
+    //
+	//var sync = $firebase(ref);
+	//var obj = sync.$asObject();
+	////
+	//$scope.myList = sync.$asArray();
+	//$scope.addBeer=function(){
+	//	//adds to your firebase
+	//	$scope.myList.$add($scope.beer);
+	//	$scope.beer= {};
+	//};
+	//$scope.removeBeer=function(){
+	//	$scope.myList.$remove($scope.beer);
+	//}
+	//
+
+});
+
+app.controller('LoginController', function($scope){
+	$scope.message('hello this is login')
+});
+
+app.controller('RegController', function($scope){
+
+});
